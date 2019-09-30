0b05f4269598cdeeeb6c88f273df7c6c0075232d
Patrick Lorio <patrick@merklex.io>
2019-09-30 16:08:17 -0700

    auto deploy

---
+++ precache-manifest.b0c71cc39548467313a2acff0698e1ed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42245c2d9b521f13d7b2a33a9fdd2614",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87a47f6495fcbc2e6aea",
+    "url": "/static/css/main.6f914edc.chunk.css"
+  },
+  {
+    "revision": "a225bce6cec62721446e",
+    "url": "/static/js/2.25003eb4.chunk.js"
+  },
+  {
+    "revision": "87a47f6495fcbc2e6aea",
+    "url": "/static/js/main.1c025c61.chunk.js"
+  },
+  {
+    "revision": "6f9220653160e10e55db",
+    "url": "/static/js/runtime~main.6a5dca77.js"
+  },
+  {
+    "revision": "29ec75d61ba733bd4630acaa1000737f",
+    "url": "/static/media/bat.29ec75d6.svg"
+  },
+  {
+    "revision": "cfb65a48aebbae19a7d184b06b3900ee",
+    "url": "/static/media/dai.cfb65a48.svg"
+  },
+  {
+    "revision": "b79033a520e2742dfd84f1af6dad812c",
+    "url": "/static/media/eth.b79033a5.svg"
+  },
+  {
+    "revision": "8593a0387b6ed53b5d0095e144b712d1",
+    "url": "/static/media/rep.8593a038.svg"
+  },
+  {
+    "revision": "a2b9c7479efa3c4dcf2494f333eac02d",
+    "url": "/static/media/usdc.a2b9c747.svg"
+  },
+  {
+    "revision": "1c8888c2357b93e235bd3f53e3885c7f",
+    "url": "/static/media/wbtc.1c8888c2.svg"
+  },
+  {
+    "revision": "9854c285f2482b2795b1066f396bde20",
+    "url": "/static/media/zrx.9854c285.svg"
+  }
+]);
